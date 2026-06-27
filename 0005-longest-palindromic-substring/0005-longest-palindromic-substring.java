5f6a3b860ce3db87cb920c6726b73cbbff6958a7
Kartik Kumbhar <kartikkumbhar1812@gmail.com>
2026-06-27 13:40:24 +0530

    Time: 2617 ms (5.01%), Space: 47.2 MB (11.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,33 @@
-public class Solution {
-    public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                String str=s.substring(i,j);
-                if(isPalindrome(str) && str.length()>=maxLen){
-                    maxLen=str.length();
-                    maxStr=str;
-                }
-            }
-        }
-
-        return maxStr;
-    }
-
-    private boolean isPalindrome(String str) {
+class Solution {
+    public boolean isPalindrome(String s) {
         int left = 0;
-        int right = str.length() - 1;
+        int right = s.length() - 1;
 
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
             left++;
             right--;
         }
-
         return true;
+    }
+
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        int max = 1;
+        String sb = s.substring(0, 1);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                boolean isPal = isPalindrome(s.substring(i, j + 1));
+                if (isPal && (j - i + 1) > max) {
+                    max = j - i + 1;
+                    sb = s.substring(i, j + 1);
+                }
+            }
+        }
+        return sb;
     }
 }
