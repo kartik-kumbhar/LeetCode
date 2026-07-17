48f62dd626c2ae6e15eb9898721e5b3fefeb38cf
Kartik Kumbhar <kartikkumbhar1812@gmail.com>
2026-07-17 14:42:50 +0530

    Time: 4 ms (74.7%), Space: 45.1 MB (61.97%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,31 +3,34 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         Stack<Integer> st = new Stack<>();
         int ans[] = new int[nums1.length];
-        for (int i = 0; i < nums2.length; i++) {
-            int c = 0;
-            for (int j = i + 1; j < nums2.length; j++) {
-                if (nums2[i] < nums2[j]) {
-                    c++;
-                    map.put(nums2[i], nums2[j]);
-                    break;
-                }
+        // for (int i = 0; i < nums2.length; i++) {
+        //     int c = 0;
+        //     for (int j = i + 1; j < nums2.length; j++) {
+        //         if (nums2[i] < nums2[j]) {
+        //             c++;
+        //             map.put(nums2[i], nums2[j]);
+        //             break;
+        //         }
+        //     }
+        //     if (c == 0) {
+        //         map.put(nums2[i], -1);
+        //     }
+        // }
+
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums2[i]>=st.peek()){
+                st.pop();
             }
-            if (c == 0) {
-                map.put(nums2[i], -1);
+            if(st.isEmpty()){
+                map.put(nums2[i],-1);
+            }else{
+                map.put(nums2[i],st.peek());
+
             }
+            st.push(nums2[i]);
         }
 
-        // for (int i = nums2.length - 1; i >= 0; i--) {
-        //     while (!st.isEmpty() && nums2[i] >= st.peek()) {
-        //         st.pop();
-        //     }
-        //     if (st.isEmpty()) {
-        //         map.put(nums2[i], -1);
-        //     } else {
-        //         map.put(nums2[i], st.peek());
-        //     }
-        //     st.push(nums2[i]);
-        // }
+        
 
         for (int i = 0; i < ans.length; i++) {
             ans[i] = map.get(nums1[i]);
