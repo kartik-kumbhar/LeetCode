932cc3b3d36acb303f216fe01e3fc3b14aec1137
Kartik Kumbhar <kartikkumbhar1812@gmail.com>
2026-08-19 11:22:44 +0530

    Time: 14 ms (15.13%), Space: 46.9 MB (65.71%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        // int result = 0;
+        // for (int n : nums) {
+        //     result ^= n;   // XOR cancels out duplicate numbers
+        // }
+        // return result;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (int num : map.keySet()) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+        return 0;
+    }
+}
